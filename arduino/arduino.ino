e284e5ee6398f41ac00c8c4c0cb633774a666201
matt lane <mattjmlane@gmail.com>
2017-03-19 17:16:48 +0000

    Merging form changes on basic arduino raspi link branches

---
+++ arduino/arduino.ino
@@ -0,0 +1,209 @@
+#include <Firmata.h>
+#include <pitches.h>
+
+// The pin numbers for the digital pins on the arduino that we are using
+// to control/read from:
+// The LED
+const int LED_PIN = 13;
+// The Button
+const int BUTTON_PIN = 9;
+// The Buzzer
+const int BUZZER_PIN = 8;
+
+// Array of notes to play with the buzzer. The constants come
+// from the 'pitches.h' file
+const int TONE_LOOP_NUM_NOTES = 8;
+const int TONE_LOOP_NOTES[TONE_LOOP_NUM_NOTES] = {
+    NOTE_C5,
+    NOTE_D5,
+    NOTE_E5,
+    NOTE_F5,
+    NOTE_G5,
+    NOTE_A5,
+    NOTE_B5,
+    NOTE_C6
+};
+
+// Firmata commands we can receive from the raspberry pi
+// These need to be consistent with the ones in the
+// raspberry pi code.
+const byte FIRMATA_REQUEST_ALERT = 1;
+const byte FIRMATA_REQUEST_BUTTON_STRING = 2;
+const byte FIRMATA_REQUEST_BUTTON_INTEGER = 3;
+// TODO challenge define a new command constant called
+// 'FIRMATA_REQUEST_PLAY_NOTE' with value 4
+const byte FIRMATA_REQUEST_PLAY_NOTE = 4;
+
+// Firmata response commands we can send to the raspberry pi
+// These need to be consistent with the ones in the
+// raspberry pi code.
+const byte FIRMATA_RESPONSE_BUTTON_STRING = 1;
+const byte FIRMATA_RESPONSE_BUTTON_INTEGER = 2;
+
+// When set to 1, sound the buzzer
+int alert = 0;
+
+// Initialise the firmata library so that we can talk to 
+// the raspberry pi
+void firmata_init(){
+    // Register the function 'firmata_sysex_callback' with
+    // the firmata library such that this function is called
+    // whenever we receive a request from the raspberry pi
+    Firmata.attach(START_SYSEX, firmata_sysex_callback);
+    // Start listening for a raspberry pi connection
+    Firmata.begin(57600);
+}
+
+// This function is here to make it easier to send 
+// response strings back to the raspberry pi
+void firmata_send_string(byte command, String message){
+    // Send a message 'message' with command 'command'
+    // to the raspberry pi
+    String msg = String(char(command)) + message;
+    int str_len = msg.length() + 1;
+    char char_array[str_len];
+    msg.toCharArray(char_array, str_len);
+    Firmata.sendString(char_array);
+}
+
+// Turn LED light on
+void turn_led_on(){
+    digitalWrite(LED_PIN, HIGH);
+}
+
+// Turn LED light off
+void turn_led_off(){
+    digitalWrite(LED_PIN, LOW);
+}
+
+// Play a specific tone on the buzzer
+// for a certain amount of milliseconds
+void play_tone(int note, int milliseconds){
+    tone(BUZZER_PIN, note, milliseconds);
+}
+
+// Function that when called repeatedly will
+// play the notes in the TONE_LOOP_NOTES array
+int current_beep_count = 0;
+int current_beep_tone = 0;
+void occasionally_beep(int beep_modulo, int beep_duration){
+    if(current_beep_count++ % beep_modulo == 0){
+        // Only play a tone once every 'beep_modulo' calls
+        // to this function
+        play_tone(TONE_LOOP_NOTES[current_beep_tone], beep_duration);
+        // Move the beep tone to the next in the array
+        // and loop back to the first note in the array
+        // if we have reached the end
+        current_beep_tone = ++current_beep_tone % TONE_LOOP_NUM_NOTES;
+    }
+    current_beep_count = current_beep_count % beep_modulo;
+}
+
+// This is the function that handles messages from the raspberry pi
+// (through the 'firmata' library over the USB link)
+void firmata_sysex_callback(byte command, byte argc, byte *argv)
+{  
+    // Perform different actions depending on which command
+    // we received from the raspberry pi
+    switch (command) {
+        byte alert_parameter;
+        case FIRMATA_REQUEST_ALERT:
+            // We received an REQUEST_ALERT command
+            // We know from the raspberry pi code that the
+            // the alert command is sent with single parameter
+            // which describes whether we the arduino should
+            // be alerted or not. This parameter is one byte long.
+            
+            alert_parameter = argv[0]; // Get the first (and only)
+            // byte from the data we received from the raspberry pi
+            
+            if (alert_parameter) {
+                alert = 1;
+                turn_led_on();
+            }
+            else {
+                alert = 0;
+                turn_led_off();
+            }
+            break;
+
+        case FIRMATA_REQUEST_BUTTON_STRING:
+            // We received a 'REQUEST_BUTTON_STRING' command.
+            // This is a request from the raspberry pi
+            // for the arduino to send a bit of text or
+            // 'string' to the raspberry pi describing
+            // the status of our button.
+            if (digitalRead(BUTTON_PIN) == HIGH) {
+                firmata_send_string(
+                    FIRMATA_RESPONSE_BUTTON_STRING,
+                    String("Button is pressed")
+                );
+            }
+            else {
+                firmata_send_string(
+                    FIRMATA_RESPONSE_BUTTON_STRING,
+                    String("Button is not pressed")
+                );
+            }
+            break;
+
+        case FIRMATA_REQUEST_BUTTON_INTEGER:
+            // We received a 'REQUEST_BUTTON_INTEGER' command.
+            // This is a request from the raspberry pi
+            // for the arduino to send an integer to the
+            // raspberry pi describing the status of our button.
+            firmata_send_string(
+                FIRMATA_RESPONSE_BUTTON_INTEGER,
+                String(digitalRead(BUTTON_PIN))
+            );
+            break;
+
+        // TODO challenge:
+        // Handle a new command called 'FIRMATA_REQUEST_PLAY_NOTE'
+        case FIRMATA_REQUEST_PLAY_NOTE:
+            // We received a 'REQUEST_PLAY_NOTE' command.
+            // This is a request from the raspberry pi
+            // for the arduino to play a tone on the buzzer
+
+            // TODO challenge:
+            // We expect two parameters, the first being an integer
+            // index into our array of notes (called TONE_LOOP_NOTES),
+            // the second being the duration for the note in tenths
+            // of seconds,
+            // These arguments come from the argv array.
+            // We want to call the play_tone function with the new tone
+            // and millisecond duration
+            
+            play_tone(TONE_LOOP_NOTES[argv[0]], argv[1] * 100);
+            
+            break;
+    }
+}
+
+// Setup function that is called when the arduino first runs this code
+void setup(){
+    // set the LED and button pins to their appropriate input/output modes 
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    // Initialise the firmata connection the raspberry pi
+    firmata_init();
+}
+
+// Loop function that is called over and over after the arduino has run the
+// setup function
+void loop(){
+    // First check if we have received any messages from the raspberry pi
+    // that we need to process
+    while(Firmata.available()) {
+        Firmata.processInput();
+    }
+    // Then do everything else
+    // In this case, if the 'alert' integer is set to a non-zero value
+    // beep the buzzer.
+    if(alert){
+        occasionally_beep(2, 20);
+    }
+    // Sleep for a bit as we don't want to rush through the buzzer's
+    // set of notes
+    delay(200);
+}
---
+++ raspberry_pi/arduino.py
@@ -0,0 +1,162 @@
+import os
+import time
+
+# For documentation on pyfirmata and the firmata protocol, go to 
+# https://media.readthedocs.org/pdf/pyfirmata/latest/pyfirmata.pdf
+# https://github.com/firmata/protocol
+# (and google)
+from pyfirmata import Arduino, STRING_DATA
+from pyfirmata.util import Iterator
+
+# Firmata commands we can send to the Arduino
+# These need to be consistent with the ones in the
+# Arduino code.
+FIRMATA_REQUEST_ALERT = 1
+FIRMATA_REQUEST_BUTTON_STRING = 2
+FIRMATA_REQUEST_BUTTON_INTEGER = 3
+# TODO Challenge create a new 'FIRMATA_REQUEST_PLAY_NOTE' command
+# with value 4
+FIRMATA_REQUEST_PLAY_NOTE = 4
+
+# Firmata response commands we can receive from the Arduino
+# These need to be consistent with the ones in the
+# Arduino code.
+FIRMATA_RESPONSE_BUTTON_STRING = 1
+FIRMATA_RESPONSE_BUTTON_INTEGER = 2
+
+
+# This class will act as our interface to the Arduino from the Raspberry PI
+class ArduinoInterface:
+    def __init__(self, config):
+        self.config = config
+        self.button_status_handler = None
+        
+    @staticmethod
+    def _autodiscover_device():
+        # This code looks for an Arduino device called '/dev/ttyACM0', failing
+        # this it looks for a device called '/dev/ttyACM1', and then 
+        # '/dev/ttyACM2' and so on...
+        # If you want to run multiple Arduinos from one Raspberry Pi, you will
+        # need to identify them manually as is done with the 
+        # self.config['device'] config item. You can find all connected devices
+        # in the terminal by typing 'ls /dev/ttyACM*'
+        device_list = ['/dev/ttyACM{}'.format(i) for i in range(10)]
+        devices = [dev for dev in device_list if os.path.exists(dev)]
+        return devices[0]
+        
+    def connect(self):
+        # Connect to Arduino
+        device = None 
+        try:
+            # If self.config['device'] is set, we'll use this as the address
+            # of the device
+            device = self.config['device']
+        except KeyError:
+            # If this config item is not set, try to 'autodiscover' the device
+            device = self.__class__._autodiscover_device()
+
+        # Connect to the Arduino
+        self.board = Arduino(device, baudrate=57600,timeout=100)
+        Iterator(self.board).start()
+
+        # Set this classes' '_handle_string_data' method
+        # as the function that handles STRING_DATA messages
+        # from the raspberry pi
+        self.board.add_cmd_handler(
+            STRING_DATA,
+            self._handle_string_data
+        )
+
+    def send_alert_update(self, alert=True):
+        # Send the REQUEST_ALERT command (1) to the Arduino 
+        # with a single byte 'alert' as a parameter
+        self.board.send_sysex(FIRMATA_REQUEST_ALERT, bytes([alert]))
+
+    def request_button_string_status(self):
+        # Send the REQUEST_BUTTON_STRING command (2) to ask the Arduino
+        # to send its current sensor data as a string.
+        # We send an empty array of bytes as we have no parameters to
+        # send along with the command.
+        self.board.send_sysex(FIRMATA_REQUEST_BUTTON_STRING, bytes([]))
+
+    def request_button_integer_status(self):
+        # Send the REQUEST_BUTTON_INTEGER command (3) to ask the Arduino
+        # to send its current sensor data as an integer.
+        # We send an empty array of bytes as we have no parameters to
+        # send along with the command.
+        self.board.send_sysex(FIRMATA_REQUEST_BUTTON_INTEGER, bytes([]))
+
+    def request_play_note(self, tone_index, duration_ms_tenths):
+        # Send the REQUEST_PLAY_NOTE command (4) to ask the Arduino
+        # to play a note in the set of notes between C5 and C6
+
+        # TODO Challenge:
+        # Send a FIRMATA_REQUEST_PLAY_NOTE command with two single-byte
+        # arguments.
+        # The first argument is an index from 0 to 7 into the
+        # array of 8 notes from C5 to C6
+        # The second argument is the duration of the note in 10ths of
+        # seconds
+        
+        self.board.send_sysex(
+            FIRMATA_REQUEST_PLAY_NOTE,
+            bytes([ tone_index, duration_ms_tenths ])
+        )
+
+    def _handle_string_data(self, *string):
+        command = None
+        try:
+            command = string[0]
+        except ValueError:
+            print('Empty command received: {}'.format(string))
+        else:
+            if command == FIRMATA_RESPONSE_BUTTON_STRING:
+                # Command code is BUTTON_STRING (1).
+                # Call the attached sensor data handler method
+                # with the sensor data (all data after the command code).
+                self.button_string_handler(bytes(string[2::2]).decode('ascii'))
+            if command == FIRMATA_RESPONSE_BUTTON_INTEGER:
+                # Command code is BUTTON_INTEGER (2).
+                # Call the attached sensor data handler method
+                # with the sensor data (all data after the command code).
+                self.button_integer_handler(int(bytes(string[2::2]).decode('ascii')))
+        
+    def attach_button_string_handler(self, func):
+        # Take the parameter function and call it whenever we receive button
+        # string data.
+        self.button_string_handler = func
+        
+    def attach_button_integer_handler(self, func):
+        # Take the parameter function and call it whenever we receive button
+        # integer data.
+        self.button_integer_handler = func
+
+
+# Run the following code 'IF and ONLY IF' this file is directly being run by the
+# Python interpreter i.e. with 'python3 arduino.py' rather than being included
+# as a module in another python file.
+if __name__ == '__main__':
+    # Create an instance of the ArduinoInterface class.
+    # Pass in an empty configuration dictionary as we want the
+    # ArduinoInterface object to discover for itself the attached Arduino
+    arduino = ArduinoInterface({})
+    arduino.connect()
+
+    # Attach some simple functions to receive the data incoming from the Arduino
+    # and then print it out
+    def handle_button_string_data(data):
+        print("Button String Data: '{}'".format(data))
+    def handle_button_integer_data(data):
+        print("Button Integer Data: {}".format(data))
+    arduino.attach_button_string_handler(handle_button_string_data)
+    arduino.attach_button_integer_handler(handle_button_integer_data)
+    
+    while True:
+        arduino.send_alert_update(alert=True)
+        time.sleep(2)
+        arduino.send_alert_update(alert=False)
+        time.sleep(2)
+        arduino.request_button_string_status()
+        arduino.request_button_integer_status()
+        arduino.request_play_note(1, 20)
+        time.sleep(5)
